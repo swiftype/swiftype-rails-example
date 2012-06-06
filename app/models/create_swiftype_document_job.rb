9f898862c189210aa6ee188a0fd704cd114a4183
Luke Francl <look@recursion.org>
2012-06-05 23:01:51 -0500

    Include URL to post in Swiftype document

--- app/models/create_swiftype_document_job.rb
+++ app/models/create_swiftype_document_job.rb
@@ -6,6 +6,7 @@ class CreateSwiftypeDocumentJob < Struct.new(:post_id)
     type.create_document({:external_id => post.id,
                            :fields => [{:name => 'title', :value => post.title, :type => 'string'},
                                        {:name => 'body', :value => post.body, :type => 'text'},
+                                       {:name => 'url', :value => Rails.application.routes.url_helpers.post_url(post, :host => "localhost:5000"), :type => 'enum'},
                                        {:name => 'created_at', :value => post.created_at.iso8601, :type => 'date'}]})
   end
 end
